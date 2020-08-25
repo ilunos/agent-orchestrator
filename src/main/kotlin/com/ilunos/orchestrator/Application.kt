9918c022d3dce8e59ea23f5db8af1d3e050fbdea
mhaenen <Marlon.Haenen@it-novum.com>
2020-08-25 10:13:09 +0200

    Add Base Application Files

---
+++ src/main/kotlin/com/ilunos/orchestrator/Application.kt
@@ -0,0 +1,11 @@
+package com.ilunos.orchestrator
+
+import io.micronaut.runtime.Micronaut.*
+
+fun main(args: Array<String>) {
+    build()
+            .args(*args)
+            .packages("com.ilunos.orchestrator")
+            .start()
+}
+
---
+++ src/test/kotlin/com/ilunos/orchestrator/AgentOrchestratorTest.kt
@@ -0,0 +1,19 @@
+package com.ilunos.orchestrator
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+
+@MicronautTest
+class AgentOrchestratorTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
