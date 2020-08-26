96bd66a74b434049feaa787ac094650e3adb0f47
mhaenen <Marlon.Haenen@it-novum.com>
2020-08-26 14:44:46 +0200

    Rename UserPasswordAuthenticationProvider to DevelopmentAuthenticationProvider and assign correct roles. Only enabled if basic-auth.development is true

--- src/main/kotlin/com/ilunos/orchestrator/security/DevelopmentAuthenticationProvider.kt
+++ src/main/kotlin/com/ilunos/orchestrator/security/DevelopmentAuthenticationProvider.kt
@@ -1,5 +1,7 @@
 package com.ilunos.orchestrator.security
 
+import io.micronaut.context.annotation.Requires
+import io.micronaut.core.util.StringUtils
 import io.micronaut.http.HttpRequest
 import io.micronaut.security.authentication.*
 import io.reactivex.Maybe
@@ -8,14 +10,15 @@ import org.reactivestreams.Publisher
 import javax.inject.Singleton
 
 @Singleton
-class UserPasswordAuthenticationProvider : AuthenticationProvider {
+@Requires(property = "micronaut.security.basic-auth.development", value = StringUtils.TRUE, defaultValue = StringUtils.FALSE)
+class DevelopmentAuthenticationProvider : AuthenticationProvider {
 
     override fun authenticate(httpRequest: HttpRequest<*>?, authenticationRequest: AuthenticationRequest<*, *>): Publisher<AuthenticationResponse> {
         return Maybe.create { emitter: MaybeEmitter<AuthenticationResponse> ->
             if (authenticationRequest.identity == "admin" && authenticationRequest.secret == "admin") {
-                emitter.onSuccess(UserDetails("admin", listOf("AGENT_ORCHESTRATOR_ADMIN", "AGENT_ORCHESTRATOR_USER")))
+                emitter.onSuccess(UserDetails("admin", listOf("AGENT_ORCHESTRATOR_EDIT", "AGENT_ORCHESTRATOR_VIEW")))
             } else if (authenticationRequest.identity == "user" && authenticationRequest.secret == "user") {
-                emitter.onSuccess(UserDetails("user", listOf("AGENT_ORCHESTRATOR_USER")))
+                emitter.onSuccess(UserDetails("user", listOf("AGENT_ORCHESTRATOR_VIEW")))
             } else {
                 emitter.onError(AuthenticationException(AuthenticationFailed()))
             }
