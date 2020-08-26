cab768c52b4ce7d15eb91baee8d390565a50015e
mhaenen <Marlon.Haenen@it-novum.com>
2020-08-26 15:04:09 +0200

    Copy Template Application.yml to config folder if not existing

--- src/main/kotlin/com/ilunos/orchestrator/Application.kt
+++ src/main/kotlin/com/ilunos/orchestrator/Application.kt
@@ -1,8 +1,13 @@
 package com.ilunos.orchestrator
 
+import com.ilunos.orchestrator.utils.FileUtils
 import io.micronaut.runtime.Micronaut.*
 
 fun main(args: Array<String>) {
+    if (!FileUtils.exists("config/application.yml")) {
+        FileUtils.copyTemplate("templates/application.yml", "config/application.yml")
+    }
+
     build()
             .args(*args)
             .packages("com.ilunos.orchestrator")
