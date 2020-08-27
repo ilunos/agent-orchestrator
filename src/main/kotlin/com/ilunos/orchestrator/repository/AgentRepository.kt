5279e8868a62fc3a7bf67a2d8f6ae86402459a9a
mhaenen <Marlon.Haenen@it-novum.com>
2020-08-27 10:11:04 +0200

    Fix AgentRepository#existsByUrl having wrong parameters

--- src/main/kotlin/com/ilunos/orchestrator/repository/AgentRepository.kt
+++ src/main/kotlin/com/ilunos/orchestrator/repository/AgentRepository.kt
@@ -4,11 +4,12 @@ import com.ilunos.orchestrator.domain.AgentInfo
 import io.micronaut.data.annotation.Query
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.repository.CrudRepository
+import java.net.URL
 
 @Repository
 interface AgentRepository : CrudRepository<AgentInfo, Long> {
 
-    fun existsByUrl(name: String): Boolean
+    fun existsByUrl(url: URL): Boolean
 
     @Query("SELECT * FROM agent WHERE enabled = true", nativeQuery = true)
     fun findAllEnabled(): List<AgentInfo>
